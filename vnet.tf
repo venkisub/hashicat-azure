66d1202377bdfe1875574d040c6f3ed7d7500423
venkisub <venkitachalam.subramanian@dhl.com>
2021-03-31 09:39:57 +0000

    ,,,

--- vnet.tf
+++ vnet.tf
@@ -1,5 +1,5 @@
 module "network" {
   source  = "app.terraform.io/venkisub-training/network/azurerm"
   version = "3.0.1"
-  resource_group_name =var.resource_group_name
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
